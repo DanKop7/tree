5734d7c1c379952d3ed004c4d69347e611d05d87
DanKop7 <dk_reg@abv.bg>
2024-09-24 17:39:38 +0300

    mam

---
+++ b/04.Tiles/Program.cs
@@ -0,0 +1,17 @@
+﻿double bathroomW = double.Parse(Console.ReadLine());
+double bathroomH = double.Parse(Console.ReadLine());
+double tileW = double.Parse(Console.ReadLine());
+double tileH = double.Parse(Console.ReadLine());
+
+double bathArea = bathroomW * bathroomH;
+bathArea = bathArea + 0.10 * bathArea;
+double tileArea = tileW * tileH;
+double totalTiles = bathArea / tileArea;
+
+Console.WriteLine(Math.Round(totalTiles));
+
+
+
+
+
+
