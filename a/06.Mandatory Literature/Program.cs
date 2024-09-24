78b7e51a26fded72b2414e43b64d5c6c9df13571
DanKop7 <dk_reg@abv.bg>
2024-09-24 17:41:36 +0300

    mam

---
+++ a/06.Mandatory Literature/Program.cs
@@ -0,0 +1,8 @@
+﻿int bookPages = int.Parse(Console.ReadLine());  
+int hourPages  = int.Parse(Console.ReadLine());
+int daysToFinish = int.Parse(Console.ReadLine());
+
+int readingTime = bookPages / hourPages;
+int hoursPerDays = readingTime / daysToFinish;
+
+Console.WriteLine(hoursPerDays);
---
+++ b/07.Supplies for School/Program.cs
@@ -0,0 +1,18 @@
+﻿int pensPack = int.Parse(Console.ReadLine());
+int markerPack  = int.Parse(Console.ReadLine());
+int literCleaner = int.Parse(Console.ReadLine());
+double discount = double.Parse(Console.ReadLine()) / 100;
+
+double pensPrice = 5.80;
+double markerPrice = 7.20;
+double cleanerPriceLiter = 1.20;
+
+double totalPens = pensPack * pensPrice;
+double totalMarker = markerPack * markerPrice;
+double totalCleaner = literCleaner * cleanerPriceLiter;
+
+double totalPrice = totalPens + totalMarker + totalCleaner;
+
+double finalPrice = totalPrice - (totalPrice * discount);
+
+Console.WriteLine(finalPrice); 
