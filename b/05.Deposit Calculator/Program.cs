3b5561b7646055a5334d0af536d8726dd9dcd5fd
DanKop7 <dk_reg@abv.bg>
2024-09-24 17:40:35 +0300

    mjm

---
+++ b/05.Deposit Calculator/Program.cs
@@ -0,0 +1,10 @@
+﻿double depositAmount = double.Parse(Console.ReadLine());
+int termDeposit = int.Parse(Console.ReadLine());
+double deannualRate = double.Parse(Console.ReadLine()) / 100;
+
+
+double amount = depositAmount + termDeposit * (depositAmount * deannualRate) / 12;
+
+Console.WriteLine($"{amount:F2}");
+
+
